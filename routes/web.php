aab0c479f67661b7e9414604792d0fce17429f6c
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2021-11-22 01:54:33 +0800

    了解Model和Collection的差異

--- routes/web.php
+++ routes/web.php
@@ -19,30 +19,39 @@ Route::get('/', function () {
 //    $post = new Post();
 //    $post->title = 'test title';
 //    $post->content = 'test content';
-//    $post->save();
+//    $post->save();  //---2-5---
 //    Post::create([
 //        'title' => 'test title',
 //        'content' => 'test content',
-//    ]);
+//    ]);   //---2-2---
 //    $posts = Post::all();
-//    dd($posts);
+//    dd($posts);  //---3-2---
 //    $post = Post::find(1);
-//    dd($post);
+//    dd($post);  //---3-3---
 //    $posts = Post::where('id','<',10)->orderBy('id','DESC')->get();
-//    dd($posts);
+//    dd($posts);  //---3-4---
 //    $post = Post::find(1);
 //    $post->update([
 //        'title' => 'updated title',
 //        'content' => 'updated content',
-//    ]);
+//    ]);  //---4-1---
 //    $post = Post::find(1);
 //    $post->title = 'saved title';
 //    $post->content = 'saved content';
-//    $post->save();
+//    $post->save();   //---4-3---
 //    $post = Post::find(1);
-//    $post->delete();
-//    Post::destroy(2);
-    Post::destroy(3,5,7);
+//    $post->delete();  //---5-1---
+//    Post::destroy(2);  //---5-3---
+//    Post::destroy(3,5,7);  //---5-5---
+
+//    $allPost = Post::all();
+//    dd($allPost);  //---6-1---
+//    $featuredPost = Post::where('is_feature',1)->get();
+//    dd($featuredPost);  //---6-2---
+//    $fourthPost = Post::find(4);
+//    dd($fourthPost);  //---6-3---
+    $lastPost = Post::orderBy('id','DESC')->first();
+    dd($lastPost);
 });
 
 Route::get('posts',[\App\Http\Controllers\PostController::class,'index'])->name('post.index');
